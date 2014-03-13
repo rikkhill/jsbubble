9e0743f48c0fce849190dd12783b3d5d7353d42a
Rikk Hill <rikk.hill@net-a-porter.com>
2014-03-13 16:12:12 +0000

    bubble adding

--- js/jsbubble.js
+++ js/jsbubble.js
@@ -7,8 +7,28 @@ requires jQuery
 */
 
 
+function randomPageLocation() {
 
+    var x = Math.floor( Math.random() * $(window).height() );
+    var y = Math.floor( Math.random() * $(window).height() );
+    return [x, y];
+}
 
+function Bubble() {
+
+    var rloc = randomPageLocation();
+
+    this.x = rloc[0];
+    this.y = rloc[1];
+
+    this.obj = $('<div>').addClass("bubble").css({
+        top	: $(window).height() - this.x,
+        left	: this.y
+    }).get();
+
+    $("body").append(this);
+
+} 
 
 
 
@@ -16,10 +36,9 @@ requires jQuery
 
 $(document).ready( function() {
 
-   $('body').append("<p>Bubbles embedded</p>")
+    console.log('jsbubble started');
 
 
-
-
+    var b = new Bubble();
 
 });
