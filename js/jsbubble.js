968cf7fb7f7523e95ede877a1fc2227260ce4305
Rikk Hill <rikk.hill@net-a-porter.com>
2014-03-14 17:28:00 +0000

    added force engine and working on collision detection

--- js/jsbubble.js
+++ js/jsbubble.js
@@ -6,84 +6,28 @@ requires jQuery
 
 */
 
+// Debugging functions
 
-// Colour functions
-
-function Colour(hex) { //object for colour, instantiated from hex value
-
-    hex = typeof hex !== "undefined" ? hex : "#XXXXXX";
-
-    this.iama = "colour";
-
-    this.red = 0;
-    this.green = 0;
-    this.blue = 0;
-
-    var hmatch = hex.match("^#([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})$");
-    if (!hmatch) { // Random colour if no match
-        this.red = randInt(0,255);
-        this.green = randInt(0,255);
-        this.blue = randInt(0,255);
+function logger(){
+    if (DEBUG){
+        console.log.apply(console, arguments);
     }
-    else {
-        this.red = parseInt(hmatch[1], 16);
-        this.green = parseInt(hmatch[2], 16);
-        this.blue = parseInt(hmatch[3], 16);
-    }
+}
 
-    this.randCol = function() { // Random colour out of nowhere
-        var component, returner = "#";
-        for (var i in [1,2,3]) {
-           component = randInt(0,255).toString(16);
-           if (component.length < 2) {
-               component = "0" + component;
-           }
-           returner += component;
-        }
-        return returner;
-    }
-
-    this.hex = function() { // return a hex value for this colour
-        var i, component, returner = '#';
-        var cols = [this.red, this.green, this.blue];
-        for (i in cols) {
-            component = cols[i].toString(16);
-            if (component.length < 2) {
-                component = "0" + component;
-            }
-            returner += component;
-        }
-        return returner;
-    }
-
-    this.mix = function(mixer) { // Take other colour and average out all values with this one
-
-        mixer = typeof mixer !=="undefined" ? mixer : randCol();
-
-        if(mixer.iama == "colour") {  // if it's not a colour object
-            mixer = mixer.hex();
-        }
-
-        var hmatch = mixer.match("^#([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})$");
-        if(hmatch){
-            this.red = (this.red + parseInt(hmatch[1], 16)) / 2; 
-            this.green= (this.green + parseInt(hmatch[2], 16)) / 2; 
-            this.blue = (this.blue + parseInt(hmatch[3], 16)) / 2; 
-        }
-        else {
-            this.red = (this.red + mixer.red) / 2; 
-            this.green= (this.green + mixer.green) / 2; 
-            this.blue = (this.blue + mixer.blue) / 2; 
-        }
-        return this.hex();
+function problogger() {   // Log this probabilistically
+    if (DEBUG && randInt(1,20) == 1){
+        console.log.apply(console, arguments);
     }
 }
 
 
-function randomPageLocation() { // Page location excepting a border of 100px
 
-    var x = randInt( 100, $(window).height() - 100 );
-    var y = randInt( 100, $(window).width() - 100 );
+// Random number functions
+
+function randomContainerLocation(obj) { // Page location excepting a border of 100px
+
+    var x = randInt( 100, $(obj).width() - 200 );
+    var y = randInt( 100, $(obj).height() - 200 );
     return [x, y];
 }
 
@@ -91,35 +35,298 @@ function randInt(a,b) {
     return Math.floor( Math.random() * (b - a + 1) + a );
 }
 
-function Bubble() {
 
-    var rloc = randomPageLocation();
+// Vector functions
+
+
+function euc(v1, v2) { // Euclidean distance between two points
+    return Math.round( Math.pow( Math.pow(v1[0] - v2[0], 2) + Math.pow(v1[1] - v2[1], 2), 0.5) );
+}
+
+function vSum(v1, v2) { // the sum of two vectors
+    return [v1[0] + v2[0], v1[1] + v2[1]];
+}
+
+function vDiff(v1, v2) { // the difference of two vectors
+    return [v1[0] - v2[0], v1[1] - v2[1]];
+}
+
+// Colour functions
+
+function rgbFromHex(hex) {  // Convert a hex colour code to an array of RGB values
+    var hmatch = hex.match("^#([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})$");
+    if (hmatch) {
+        return [  parseInt(hmatch[1], 16), parseInt(hmatch[2],16), parseInt(hmatch[3], 16)];
+    }
+    else {
+        return false;
+    }
+}
+
+function hexFromRgb(rgb) {  // Convert an array of RGB values to a hex colour code
+    var i, component, returner = '#';
+    for (i in rgb) {
+        if(rgb[i] > 255) {
+            rgb[i] -= 255;
+        }
+        component = rgb[i].toString(16);
+        if (component.length < 2) {
+            component = "0" + component;
+        }
+        returner += component;
+    }
+    return returner;
+
+}
+
+function blendCol(a, b) {  // Take the mean average of two colours
+
+   var acol, bcol, blend = [];
+   acol = rgbFromHex(a);
+   bcol = rgbFromHex(b);
+   
+   for (i in acol) {
+       blend[i] = Math.ceil((acol[i] + bcol[i]) / 2);
+   }
+   
+   return hexFromRgb(blend);
+}
+
+function randCol() { // Completely random colour
+    var component, returner = "#";
+    for (var i in [1,2,3]) {
+       component = randInt(0,255).toString(16);
+       if (component.length < 2) {
+           component = "0" + component;
+       }
+       returner += component;
+    }
+    return returner;
+}
+
+function ForceEngine() {  // Applies forces to particles and manages collisions
+  
+    var self = this;
+    
+    this.particles = [];
+    this.forces = [];
+    this.obstacles = [];
+    
+    this.timer; 
+    
+    this.tick_size = 42;  // Number of ms in a "tick"
+    
+    this.absolute_time = 0;  // absolute time
+    
+
+    this.addParticle = function(particle) {  // Add a particle
+   
+        particle.appear();
+        this.particles.push(particle);
+    }
+    
+    this.addForce = function(force) {  // Add a particle
+    
+        this.forces.push(force);
+
+    }
+    
+    this.addObstacle = function(obstacle) { // Add a constraint / wall
+        this.obstacles.push(obstacle);
+    }
+    
+    this.tick = function() {  // apply forces to particles and check for collision
+        var x, y, r, t, i, j;
+        for (i in self.particles) {
+            x = self.particles[i].x;
+            y = self.particles[i].y;
+            r = self.particles[i].radius;
+            t = self.absolute_time;
+        
+            if (self.particles[i].ghost!== false) {
+                continue;
+            }
+
+            self.particles[i].x += self.particles[i].v_i;
+            self.particles[i].y -= self.particles[i].v_j;
+            self.particles[i].render();
+            
+            // Forces
+            
+            for (j in self.forces) {
+
+                self.particles[i].exert(self.forces[j](x, y, t));
+            }
+            
+            // Collisions
+            
+            for (j in self.obstacles) {
+            
+                logger(r, x, y, self.obstacles[j].distance(x, y));
+                
+            
+                if (self.obstacles[j].distance(x, y) < r) {
+                    logger(self.particles[i].id + " hit " + self.obstacles[j].id);
+                }
+            }
+            
+        }
+
+        self.absolute_time += self.tick_size;
+    }
+    
+    this.start = function() {
+        this.timer = setInterval(this.tick, this.tick_size);// interval);
+        logger("timer started");
+        return true;
+    }
+    
+    this.stop = function() {
+        clearInterval(this.timer);
+        logger("timer stopped");  
+        return true;
+    }
+
+}
+
+function Wall(id, a, b, c) {  // We define a line in the plane to test collisions off
+    
+    this.id = id;
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    
+    this.distance = function(x0, y0) { // Distance from this line
+       return Math.abs((this.a*x0)+(this.b*y0)+c)/Math.sqrt(Math.pow(this.a,2)+Math.pow(this.b,2));
+    }
+}
+
+function Bubble() {  // A large round colourful particle
+
+    var self = this;
+
+    var rloc = randomContainerLocation(CONTAINER);
+
+    this.radius = randInt(10,35);
+    this.mass = this.radius * this.radius; // Math.pow(this.radius, 0.5); // approximate "mass" relative to size   
+    this.ghost = true;
+        
+    this.id = randInt(10000, 99999);
 
     this.x = rloc[0];
     this.y = rloc[1];
+    
+    
+    this.v_i = -2;
+    this.v_j = 0;
 
-    this.size = randInt(10,80);
+    
 
-    this.obj = $('<div>').addClass("bubble").css({
-        top	: $(window).height() - (this.x + (this.size/2)),
-        left	: this.y -(this.size/2) ,
-        height	: this.size,
-	width	: this.size
+    
+    this.trans = 0.5; // How opaque is this particle?
+    
+    this.colour = blendCol(randCol(), '#AAAA22');
+
+    this.obj = $('<div>').addClass("bubble").attr("id", toString(this.id)).css({
+        'left'		: this.x - (this.radius) ,
+        'top'		: $(CONTAINER).height() - (this.y + (this.radius)),
+        'height'	: this.radius * 2,
+	'width'		: this.radius * 2,
+        'position'	: 'absolute',
+        'border-radius' : this.radius,
+        'opacity'	: this.trans,
+        'background-color': this.colour
     }).html("&nbsp;");
+    
+    logger("bubble " + this.id + " instantiated");
+    
+    this.appear = function() { // Fade particle in
+        logger("bubble " + this.id + " placed");
+        $(this.obj).css('opacity', 0);
+        $("body").append(this.obj);
+        $(this.obj).fadeTo(randInt(100, 1200), this.trans, function(){ self.substantiate() });
+    }
+    
+    this.render = function() {  // apply css based on properties
+        $(this.obj).css({
+        'left'		: this.x - (this.radius),
+        'top'		: $(CONTAINER).height() - (this.y + (this.radius)),
+        'opacity'	: this.trans,
+        'background-color': this.colour
+        });
+    }
+    
+    this.exert = function(vector){  // Exert a force vector on this particle
+        this.v_i += Math.ceil(vector[0] / this.mass);
+        this.v_j += Math.ceil(vector[1] / this.mass);
+    }
+    
+    this.reflect = function(vector) { // reflect off given position vector
+        var diff = vDiff (vector, self.pos());
+        
+        this.v_i = this.v_i * (diff[0] / Math.abs(diff[0]) );
+        this.v_j = this.v_j * (diff[1] / Math.abs(diff[1]) );
 
-    $("body").append(this.obj);
-    console.log("bubble blown!");
+    }
+    
+    this.substantiate = function() {
+        this.ghost = false;
+    }
+    
+    this.pos = function(position) {  // set / return position vector
+    
+        if (typeof position !== "undefined") {
+            this.x = position[0];
+            this.y = position[1];
+        }
+        
+        return [this.x, this.y];
+    }
+    
+    this.vel = function(velocity) { // set / return velocity vector
+    
+        if (typeof velocity !== "undefined") {
+            this.v_i = velocity[0];
+            this.v_j = velocity[1];
+        }
+        
+        return [this.v_i, this.v_j];   
+    }
+
+
+    
+
 } 
 
+// Globals
 
+DEBUG = true;
 
-
+CONTAINER = window;
 
 $(document).ready( function() {
 
-    console.log('jsbubble started');
 
+    world = new ForceEngine();
+    
+    for (var i = 0; i < 1; i++) {
+        world.addParticle(new Bubble());
+    }
+    
+    force1 = function(x,y,t) { // Forcefields are functions
+   
+        //return [10 * Math.sin(y),10 * Math.sin(x)];
+        return [1, 0];
+    }
+    
+    wall1 = new Wall("left wall", 100, 0, -100); // Wall at x = 100
+    
+    //wall1 = new Wall("right wall", $(CONTAINER).width() - 100, 0, $(CONTAINER).width() - 100); // Wall at x = 100
+        
+    //world.addForce(force1);
+    world.addObstacle(wall1);
+    
+    world.start();
 
-    var b = new Bubble();
 
 });
