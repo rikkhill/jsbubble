b31986daa55d36a9a82fb082b2ca92776f4d4743
Rikk Hill <rikk@unashamedly.co.uk>
2014-03-16 19:28:59 +0000

    Working, no collision tree system

--- js/jsbubble.js
+++ js/jsbubble.js
@@ -26,7 +26,83 @@ function randInt(a,b) {
     return Math.floor( Math.random() * (b - a + 1) + a );
 }
 
-// Super simple particle
+function randLocation(container) {
+    var x = $(container).width();
+    var y = $(container).height();
+    return [ randInt(0, x), randInt(0,y)   ];
+}
+
+
+
+// Colour functions
+
+function rgbFromHex(hex) {  // Convert a hex colour code to an array of RGB values
+    var hmatch = hex.match("^#([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})([0-9a-fA-F]{1,2})$");
+    if (hmatch) {
+        return [  parseInt(hmatch[1], 16), parseInt(hmatch[2],16), parseInt(hmatch[3], 16)];
+    }
+    else {
+        return false;
+    }
+}
+
+function hexFromRgb(rgb) {  // Convert an array of RGB values to a hex colour code
+    var i, component, returner = '#';
+    for (i in rgb) {
+        if(rgb[i] > 255) {
+            rgb[i] -= 255;
+        }
+        component = rgb[i].toString(16);
+        if (component.length < 2) {
+            component = "0" + component;
+        }
+        returner += component;
+    }
+    return returner;
+
+}
+
+function blendCol(a, b) {  // Take the mean average of two colours
+
+    var acol, bcol, blend = [];
+    acol = rgbFromHex(a);
+    bcol = rgbFromHex(b);
+    
+    for (i in acol) {
+        blend[i] = Math.round((acol[i] + bcol[i]) / 2);
+    }
+   
+    return hexFromRgb(blend);
+}
+
+function weightCol(a, b, wa, wb) { // take two colours and return the weighted mean
+
+    var acol, bcol, blend = [];
+    acol = rgbFromHex(a);
+    bcol = rgbFromHex(b);
+    
+    for (i in acol) {
+        blend[i] = Math.round(  (   ( wa * acol[i]) + (wb * bcol[i])  ) / (wa + wb)   );
+
+    }
+    
+    return hexFromRgb(blend);
+}
+
+function randCol() { // Completely random colour
+    var component, returner = "#";
+    for (var i in [1,2,3]) {
+       component = randInt(0,255).toString(16);
+       if (component.length < 2) {
+           component = "0" + component;
+       }
+       returner += component;
+    }
+    return returner;
+}
+
+
+// Particle Stuff
 
 
 function World() {
@@ -40,7 +116,7 @@ function World() {
     this.obs = [];    // array of obstacles
 
     this.timer; 
-    this.tick_size = 33;  // Number of ms in a "tick"
+    this.tick_size = 41;  // Number of ms in a "tick"
 
     this.absolute_time = 0;  // absolute time
 
@@ -71,6 +147,12 @@ function World() {
                     self.obs[j].impact(self.parts[i]);
                 }
             
+            }
+            
+            for (j in self.parts) {  // check for other particles
+                if(self.parts[i].distanceFrom(self.parts[j]) <= self.parts[i].radius + self.parts[j].radius) {  // Simple particle collision detection
+                    self.parts[i].collide(self.parts[j]);
+                }
             }
         }
         
@@ -121,8 +203,6 @@ function Wall(nick, anchor, vector) {
     
     this.impact = function(particle) {  // Adjust a colliding particle's properties appropriately
     
-        logger("particle hit wall " + nick);
-    
         var pos = $V(particle.pos()).to3D();
         var vel = $V(particle.vel()).to3D();
         var impact_point = this.line.pointClosestTo(pos);
@@ -135,7 +215,6 @@ function Wall(nick, anchor, vector) {
         
         // Adjust particle's velocity
         var proj = vel.dot(norm);
-        console.log(norm.inspect());
         var refl = vel.subtract(  norm.x(proj * 2) );
         particle.vel( [ refl.e(1), refl.e(2) ] );
     }
@@ -143,27 +222,31 @@ function Wall(nick, anchor, vector) {
 }
 
 
-function Particle(x, y, v_i, v_j, radius) {
+function Particle(d, v_i, v_j, radius) {
 
-    this.x = x;
-    this.y = y;
+    this.x = d[0];
+    this.y = d[1];
     this.v_i = v_i;
     this.v_j = v_j;
     this.radius = radius;
+    this.trans = 0.6;
     
-    this.mass = Math.sqrt(radius);
+    this.colour =   blendCol(randCol(),"#FFFFFF");
+    
+    this.mass = Math.pow(radius, 0.5);
     
     this.friction = 0;
     this.elast = 0;
     
     this.element = $("<div>").css({
         "position"		: "absolute",
-        "background-color" 	: "#9966BB",
-        "border-radius"		: this.radius,
-        "width"			: this.radius * 2,
-        "height"		: this.radius * 2,
+        "background-color" 	: this.colour,
+        "border-radius"		: (this.radius + 1),
+        "width"			: (this.radius + 1) * 2,
+        "height"		: (this.radius + 1) * 2,
         "top"			: this.y - this.radius,
-        "left"			: this.x - this.radius
+        "left"			: this.x - this.radius,
+        "box-shadow"		: "0px 0px 20px inset"  // I am going to hell for using this CSS property
     
     });
     
@@ -174,10 +257,12 @@ function Particle(x, y, v_i, v_j, radius) {
     
     this.render = function() {
         $(this.element).css({
-        "width"			: this.radius * 2,
-        "height"		: this.radius * 2,
-        "top"			: this.y - this.radius,
-        "left"			: this.x - this.radius            
+        "width"			: (this.radius + 1) * 2,
+        "height"		: (this.radius + 1) * 2,
+        "background-color" 	: this.colour,
+        "top"			: this.y - (this.radius + 1),
+        "left"			: this.x - (this.radius + 1),
+        "opacity"		: this.trans
         
         });
     }
@@ -197,7 +282,55 @@ function Particle(x, y, v_i, v_j, radius) {
         }
         
         return [this.v_i, this.v_j];   
-    }    
+    }
+    
+    this.distanceFrom = function(particle) {
+        return $V(this.pos()).distanceFrom($V(particle.pos()));
+    }
+    
+    this.collide = function(particle) {  // Adjust two colliding particles
+         var this_d = $V([this.x, this.y]);
+         var that_d = $V([ particle.x, particle.y ]);
+         
+         var this_v = $V([this.v_i, this.v_j]);
+         var that_v = $V([ particle.v_i, particle.v_j]);
+         
+         
+         var norm = this_d.subtract(that_d).toUnitVector();
+         
+         // Push each other apart so they're no longer penetrating
+         var penetration = 1 + this.radius + particle.radius - this_d.distanceFrom(that_d);
+         var bounce_share = (this.mass * this_v.modulus()) / ((this.mass * this_v.modulus()) + (particle.mass * that_v.modulus()));
+         var this_newpos = this_d.add(norm.x(penetration * bounce_share));
+         var that_newpos = that_d.subtract(norm.x(penetration * (1 - bounce_share)));
+         this.pos([ this_newpos.e(1), this_newpos.e(2)  ]);
+         particle.pos([ that_newpos.e(1), that_newpos.e(2)  ]);
+         
+         // reflect them properly
+         
+         var this_proj = this_v.dot(norm);
+         var that_proj = that_v.dot(norm);
+         
+         var impulse = ( 2.0 * (this_proj - that_proj) ) / (this.mass + particle.mass);
+         if (impulse > 0) {
+             impulse = 0;
+         }
+
+         var this_scaled_diff = this_v.subtract(norm.x(impulse * particle.mass));
+         
+         var that_scaled_diff = that_v.subtract(norm.x(-impulse * this.mass));
+         
+         this.vel([this_scaled_diff.e(1), this_scaled_diff.e(2)]);
+         
+         particle.vel([that_scaled_diff.e(1), that_scaled_diff.e(2)]);
+         
+         // Funky colour things
+         
+         //var col = weightCol(this.colour, particle.colour, this.mass, particle.mass);
+         //this.colour = blendCol(randCol(),"#FFFFFF");
+         //particle.colour = blendCol(randCol(),"#000000");
+         
+    }
 }
 
 
@@ -212,27 +345,37 @@ DIMS = {};
 
 $(document).ready( function() {
 
-    CONTAINER = $('#bubblebox').get();
+    CONTAINER = $("#bubblebox").get();
     DIMS.x = $(CONTAINER).offset().x;
     DIMS.y = $(CONTAINER).offset().y;
     DIMS.w = $(CONTAINER).width();
     DIMS.h = $(CONTAINER).height();
     
-    bubs = new Particle(200, 200, -4, -1, 15);
+    console.log(DIMS);
+    
+    //bubs = new Particle(randLocation(CONTAINER), 0.5, 0.5, 30);
+    world = new World();    
+    for (var i = 0 ; i < 30 ; i++) {
+        world.addParticle( new Particle(randLocation(CONTAINER), Math.random() * randInt(1,5), Math.random() * randInt(1,5), randInt(30,50)) );
+    
+    }
     
     wall1 = new Wall("lefty", [0,0], [0,1]); // Wall on the y-axis
     wall2 = new Wall("toppy", [0,0], [1,0]);
     
     wall3 = new Wall("righty", [DIMS.w,0], [0,1]);
     wall4 = new Wall("bottomy", [0,DIMS.h], [1,0]);
+    wall5 = new Wall("thingy", [0,0], [1,1]);
     
-    world = new World();
+
     
-    world.addParticle(bubs);
+    //world.addParticle(bubs);
     world.addObstacle(wall1);
     world.addObstacle(wall2);
     world.addObstacle(wall3);
     world.addObstacle(wall4);
+    //world.addObstacle(wall5);
+    
     
     world.start();
     
